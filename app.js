77ddf8d00a49beecff896d60950e273ca7cc30df
cosmin petre <petre.cosmin1997@gmail.com>
2022-02-07 13:43:47 +0200

    web-app build

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "web-app/build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "web-app/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/build", "index.html"));
 });
 
 app.use(require("cors")());
